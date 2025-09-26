6d5fe99880a433c31f98e12a0c43f3adc9bce815
ryan0koch <ryan_koch@mines.edu>
2025-09-26 02:23:40 -0600

    Psych I forgot to git add fan too so here it is

---
+++ src/levelPieces/Fan.java
@@ -0,0 +1,18 @@
+package levelPieces;
+
+import gameEngine.Drawable;
+import gameEngine.InteractionResult;
+
+public class Fan extends GamePiece{
+	public Fan(int location){
+		super('F',"Fan",location);
+	}
+	
+	@Override
+	public InteractionResult interact(Drawable [] gameBoard, int playerLocation) {
+		if(this.getLocation() -1 == playerLocation || this.getLocation() -2 == playerLocation) {
+			return InteractionResult.GET_POINT;
+		}else return InteractionResult.NONE;
+	}
+
+}
