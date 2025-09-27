a6d452e81158006e57a7e214329c8bd4c1a8c490
Yamato Matsumura <yamtheyam104@gmail.com>
2025-09-26 22:15:17 -0600

    added logic for TV

--- src/levelPieces/TV.java
+++ src/levelPieces/TV.java
@@ -1,5 +1,20 @@
 package levelPieces;
 
-public class TV {
+import gameEngine.Drawable;
+import gameEngine.InteractionResult;
 
+public class TV extends GamePiece{
+	
+	public TV (int location) {
+		super('T', "TV", location);
+	}
+	
+	@Override
+	public InteractionResult interact(Drawable[] gameboard, int playerLocation) {
+		if(this.getLocation() + 4 == playerLocation || this.getLocation() - 4 == playerLocation) {
+			return InteractionResult.GET_POINT;
+		} else {
+			return InteractionResult.NONE;
+		}
+	}
 }
