5dc376c8a390d79a1c2d47e9d419eff72bffdff1
Yamato Matsumura <yamtheyam104@gmail.com>
2025-09-26 23:35:05 -0600

    added comments for readability

--- src/levelPieces/MoveHelper.java
+++ src/levelPieces/MoveHelper.java
@@ -1,7 +1,10 @@
 package levelPieces;
 import gameEngine.GameEngine;
+
 public class MoveHelper {
 	public static boolean onBoard(int loc) {
+		
+		// Check if exceeds board size limit or in negative board index
 		if(loc> GameEngine.BOARD_SIZE -1 || loc < 0) {
 			return false;
 		}else return true;
